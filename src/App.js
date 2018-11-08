5ede757de114aaf1cb6a0d3471dca1f21b45f6c5
REDHEAL03\User <dhanya@redheal.com>
2018-11-08 17:56:15 +0530

    App practices

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Test from './Test';
 
 class App extends Component {
+
+  static defaultProps =  {
+    bandaiahArray : ['sairam']
+  }
+
+  static displayName = "sairam"; 
   render() {
+    console.log("default props", this.props)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>HEllo sairam rams</h1>
+
+        <Test />
       </div>
     );
   }
---
+++ src/Test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div>Sairam sana in test component</div>
+    );
+}
+
+Test.displayName = "sairamcomponent";
+
+export default Test;
